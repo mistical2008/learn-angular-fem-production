16d0bfa80927e156d4fd7172bcd22324044892f6
Evgeniy Talko <etalko@gmail.com>
2021-09-29 17:41:56 +0300

    Created basic data for work

---
+++ apps/dashboard/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreDataModule } from '@fem-production-angular/core-data';
+import { CoreStateModule } from '@fem-production-angular/core-state';
+import { MaterialModule } from '@fem-production-angular/material';
+import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component';
+import { RoutingModule } from './routing.module';
+import { WidgetDetailsComponent } from './widgets/widget-details/widget-details.component';
+import { WidgetsListComponent } from './widgets/widgets-list/widgets-list.component';
+import { WidgetsComponent } from './widgets/widgets.component';
+
+@NgModule({
+  declarations: [AppComponent, WidgetsComponent, WidgetsListComponent, WidgetDetailsComponent],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    StoreModule.forRoot({}, {}),
+    CoreDataModule,
+    CoreStateModule,
+    MaterialModule,
+    RoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule { }
